62bac7bc46740371a508070487d11f09fa6e66f6
AllanJiang17 <allanjiang17@gmail.com>
2022-01-15 22:15:11 -0800

    "added some dependency"

--- src/main/java/Jobyssey/backend/controller/JobController.java
+++ src/main/java/Jobyssey/backend/controller/JobController.java
@@ -4,8 +4,6 @@ import Jobyssey.backend.model.Company;
 import Jobyssey.backend.model.User;
 import Jobyssey.backend.services.Services;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
